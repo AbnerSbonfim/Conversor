cdfb313d0a853fc4dac06729d5c70fc088b08eaa
AbnerSbonfim <abnersalomao7bonfim@gmail.com>
2026-09-14 19:55:44 -0300

    update

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const convertButton = document.querySelector(".convert-button");
+const currencySelect = document.querySelector(".currency-select");
 
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".currency-input").value; 
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert");//Valor em Real
     const currencyValueConverted = document.querySelector(".currency-value");//Valor em outras moedas
 
+    console.log(currencySelect.value);
     const dollarToday = 5.2;
+    const euroToday = 5.9;
 
     const convertedValue = inputCurrencyValue / dollarToday;
 
